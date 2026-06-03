df10e063826e5de899fb3b9a21875be1e05802ba
mashigasri1777 <mashigasri1777@gmail.com>
2026-06-03 20:31:15 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
-from flask import Flask, jsonify
+from flask import Flask, render_template, request, jsonify
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Flask is working!"
+    return render_template("index.html")
 
 @app.route("/ask", methods=["POST"])
 def ask():
